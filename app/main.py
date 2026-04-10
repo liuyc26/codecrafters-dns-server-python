b7cd023e1c244628417c573bff42ff1ec032050b
liuyc26 <liuyc26@gmail.com>
2026-04-10 00:02:00 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
 import socket
 
 
+def parse_question_section(buf: bytes, qdcount: int) -> bytes:
+    offset = 12
+
+    for _ in range(qdcount):
+        # QNAME is a sequence of length-prefixed labels terminated by a zero byte.
+        while buf[offset] != 0:
+            offset += 1 + buf[offset]
+
+        offset += 1  # null terminator
+        offset += 4  # QTYPE (2 bytes) + QCLASS (2 bytes)
+
+    return buf[12:offset]
+
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -14,6 +28,7 @@ def main():
         try:
             buf, source = udp_socket.recvfrom(512)
 
+            # header section
             packet_id = int.from_bytes(buf[0:2], byteorder="big") # 16 bits - Packet Identifier (ID): A random ID assigned to query packets. Response packets must reply with the same ID.
             flags = int.from_bytes(buf[2:4], byteorder="big") #  1 bit - Query/Response Indicator (QR), 4 bits - Operation Code (OPCODE), 1 bit - Authoritative Answer (AA), 1 bit - Truncation (TC), 1 bit - Recursion Desired (RD), 1 bit - Recursion Available (RA), 3 bits - Reserved (Z), 4 bits - Response Code (RCODE)
             qdcount = int.from_bytes(buf[4:6], byteorder="big") # 16 bits - Question Count (QDCOUNT): Number of questions in the Question section.
@@ -23,6 +38,9 @@ def main():
 
             flags |= 1 << 15 # set to response flag
 
+            # question section
+            question_section = parse_question_section(buf, qdcount)
+
             response = (
                 packet_id.to_bytes(2, byteorder="big")
                 + flags.to_bytes(2, byteorder="big")
@@ -30,6 +48,7 @@ def main():
                 + ancount.to_bytes(2, byteorder="big")
                 + nscount.to_bytes(2, byteorder="big")
                 + arcount.to_bytes(2, byteorder="big")
+                + question_section
             )
     
             udp_socket.sendto(response, source)
